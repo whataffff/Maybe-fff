f8fe4bf7aba04e3ec1c1c0ce40fe4e479ec99150
Maksim <example@gmail.com>
2022-12-02 21:22:27 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+
+
+
+
+    
+
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
